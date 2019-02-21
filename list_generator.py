442caedbb4f0f8db54b58a02747446a4cf78cc6b
whenjohn <nguyenjohn@gmail.com>
2019-02-21 09:37:52 -0600

    Updated list gen to use random.shuffle

--- list_generator.py
+++ list_generator.py
@@ -3,7 +3,7 @@
 """IS 211 - Assignment 4"""
 
 import random
-# Import for logging
+
 
 def generateList(num_of_elements, case_scenario):
     """Generate list
@@ -11,23 +11,26 @@ def generateList(num_of_elements, case_scenario):
     Args:
         num_of_time (int): The number of times to add a new input to list
         case_scenario (int): Best (1) - sorted list, avg (2) - half sort, worst (3) - no sort case scenerio
+
     Returns:
         Returns new list
 
     """
-    new_list1 = []
-    new_list2 = []
+    list_full = []
     half_num_of_elements = num_of_elements / 2
 
-    some_random = random.randint(3,57)
+    for i in range(num_of_elements):
+        list_full.append(i)
 
-    for i in range(half_num_of_elements):
-            new_list1.append(random.randint(1, num_of_elements+1))
-            new_list2.append(random.randint(1, num_of_elements+1))
+    list_first_half = list_full[:(half_num_of_elements)]
+    list_second_half = list_full[(half_num_of_elements):]
 
     if case_scenario == 1:
-        return sorted(new_list1 + new_list2)
+        return list_first_half + list_second_half
     if case_scenario == 2:
-        return sorted(new_list1) + new_list2
+        random.shuffle(list_second_half)
+        return list_first_half + list_second_half
     if case_scenario == 3:
-        return new_list1 + new_list2
+        new_list = list_first_half + list_second_half
+        random.shuffle(new_list)
+        return new_list
